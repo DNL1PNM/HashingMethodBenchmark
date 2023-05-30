a62e42ab9736fd27e2f10fdea935cb12d75d1e57
Daniil <babosinko@gmail.com>
2023-05-30 08:48:11 +0500

    Comm1

---
+++ src/main/java/org/benchmark/CustomCRC32.java
@@ -0,0 +1,11 @@
+package org.benchmark;
+import java.util.zip.CRC32;
+
+public class CustomCRC32<K> implements HashFunc<K> {
+    public int hash(K key) {
+        CRC32 crc32 = new CRC32();
+        byte[] bytes = key.toString().getBytes();
+        crc32.update(bytes);
+        return (int) crc32.getValue();
+    }
+}
