23f7cf3ee2d1e469d71fdaec396634fee140391d
Daniil <babosinko@gmail.com>
2023-06-01 22:41:21 +0500

    01.06.23-4

--- src/main/java/org/benchmark/Main.java
+++ src/main/java/org/benchmark/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
         PseudoBenchmark<String, Integer> pseudoBenchmark = new PseudoBenchmark<>();
         pseudoBenchmark.run();
-
 //        ChainingBenchmark<String, Integer> chainingBenchmark = new ChainingBenchmark<>();
 //        chainingBenchmark.run();
 //
